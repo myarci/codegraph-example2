812d7dac1857fbae290db818029f3518e74de788
Metin Yarci <metin.yarci@aurea.com>
2019-06-06 14:02:13 +0300

    new service added

---
+++ src/main/java/hello/MyService.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyService {
+
+    public void myMethod() {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+
+}
+
